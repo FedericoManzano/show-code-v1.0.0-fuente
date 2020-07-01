bd2e309c5f48da8466512adafa3d62101e2d26a4
FedericoManzano <federicomanzano111@gmail.com>
2020-07-01 19:03:08 -0300

    CSS avanzado

--- src/modulos/CodigoCss.js
+++ src/modulos/CodigoCss.js
@@ -10,15 +10,102 @@ import $ from "jquery"
         return codigo[pos] === '@'
     }
 
+    const esCadena = (codigo, pos) => {
+        return codigo[pos] === '"' || codigo[pos] === "'"
+    }
+
+    const esProp = (codigo, pos) => {
+        return codigo[pos] === '{'
+    }
+
+    const esSelector = (codigo, pos) => {
+        if(codigo[pos] === ' ' || codigo[pos] === '\t') return false
+        return /(\.*\#*[^ @ ][a-zA-Z-_\(\)\[\])]+ *\n*{)/.test(codigo.substring(pos, codigo.length))
+    }
+
     const colorearClave = (codigo, resultado, pos) => {
         while(codigo[pos] !== ' ' && pos < codigo.length ) {
-            resultado += "<span class='show-clave'>" + codigo[pos] + "</span>"
+            resultado += "<span class='show-claves'>" + codigo[pos] + "</span>"
+            pos ++
+        }
+        resultado += codigo[pos]
+        indice = pos
+        return resultado
+    }
+
+    const colorearPop = (codigo, resultado, pos) => {
+        resultado += "<span class='show-llaves'>" + codigo[pos] + "</span>"
+        pos ++
+
+        while(codigo[pos] !== '}' && pos < codigo.length ) {
+            while(codigo[pos] !== ':' && codigo[pos] !== '}' &&pos < codigo.length ) {
+                resultado += "<span class='show-prop'>" + codigo[pos] + "</span>"
+                pos ++
+            }
+            while( codigo[pos] !== '\n' && codigo[pos] !== '}' && pos < codigo.length ) {
+                resultado += "<span class='show-valor'>" + codigo[pos] + "</span>"
+                pos ++
+            }
+        }
+
+
+        
+        resultado += codigo[pos]
+        indice = pos
+        return resultado
+    }
+
+
+    const colorearCadena = (codigo, resultado, pos) => {
+        let caracterFin = codigo[pos]
+        resultado += "<span class='show-string'>" + codigo[pos] + "</span>"
+        pos ++
+        while(codigo[pos] !== caracterFin && pos < codigo.length ) {
+            resultado += "<span class='show-string'>" + codigo[pos] + "</span>"
+            pos ++
+        }
+        if(codigo[pos] === caracterFin) 
+            resultado += "<span class='show-string'>" + codigo[pos] + "</span>"
+        else 
+            resultado += codigo[pos]
+
+        if(codigo[pos] === '}') {
+            resultado += "<span class='show-llaves'>" + codigo[pos] + "</span>"
             pos ++
         }
 
         indice = pos
         return resultado
     }
+
+    const colorearSelector = (codigo, resultado, pos) => {
+        while(codigo[pos] !== '}' && !esProp(codigo, pos) && !esClave(codigo, pos) && !esCadena(codigo, pos) && codigo[pos] !== '{' && pos < codigo.length ) {
+            resultado += "<span class='show-selectores'>" + codigo[pos] + "</span>"
+            pos ++
+        }
+
+        if(esCadena(codigo, pos) ) {
+            indice = pos
+            return resultado
+        } 
+
+        if(esClave(codigo, pos) ) {
+            indice = pos
+            return resultado
+        } 
+
+        if(esProp(codigo, pos) ) {
+            indice = pos
+            return resultado
+        }
+
+        
+        resultado += codigo[pos]
+        indice = pos
+        return resultado
+    }
+
+
 
     const inicializar = () => {
         $(".cod-css").each((index, e) => {
@@ -27,12 +114,29 @@ import $ from "jquery"
 
             let resultado = ""
             for (let i = 0; i < codigo.length; i++) {
-                if(esClave(codigo,i)){
-                    resultado = colorearClave(codigo, resultado, i)
+
+                if(esSelector(codigo, i)) {
+                    resultado = colorearSelector(codigo, resultado, i)
                     i = indice
+                    if(esCadena(codigo, i)) {
+                        resultado = colorearCadena(codigo, resultado, i) 
+                        i = indice
+                    }
+                    if(esClave(codigo, i)) {
+                        resultado = colorearClave(codigo, resultado, i) 
+                        i = indice
+                    }
+
+                    if(esProp(codigo, i)) {
+                        console.log("pror")
+                        resultado = colorearPop(codigo, resultado, i) 
+                        i = indice
+                    }
+                }
+                else {
+                    resultado += codigo[i]
                 }
             }
-
             $(e).html(resultado)
         })
     }
