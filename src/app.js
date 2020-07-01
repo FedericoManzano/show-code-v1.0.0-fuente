bef828775764404e4268f879d82dc2f5ed6146db
FedericoManzano <federicomanzano111@gmail.com>
2020-07-01 15:42:19 -0300

    Html

--- src/app.js
+++ src/app.js
@@ -1,1 +1,14 @@
 import CodigoHtml from "./modulos/CodigoHtml"
+
+(function() {
+    const ShowHtml = (conf) => {
+        CodigoHtml.iniciar(conf)
+    }
+    const Show = {
+        ShowHtmlInit: (conf) => ShowHtml(conf)
+    }
+
+    window.Show = Show
+})()
+
+export default Show
